7fc63aa1d9b05b8d4b91be783cd5751035b4a8ca
sl.truman <sltruman@localhost.localdomain>
2020-06-30 17:39:01 +0800

    14.加入bootstrap&vue.js

--- 14.video-meeting/server/www/js/index.js
+++ 14.video-meeting/server/www/js/index.js
@@ -1,3 +1,19 @@
+const Vue = require('vue')
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  },
+})
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: '页面加载于 ' + new Date().toLocaleString()
+  }
+})
+
 var axios = require('axios').default
 
 //创建会议

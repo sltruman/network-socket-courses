dfd623b1f77d7cfc16c8d4f09cab022a82ac6999
Truman <sl.truman@live.com>
2020-07-05 23:09:57 +0800

    14.准备会议界面加入VueJS

--- 14.video-meeting/server/www/js/index.js
+++ 14.video-meeting/server/www/js/index.js
@@ -1,47 +1,55 @@
 const Vue = require('vue')
+const axios = require('axios')
 
-var app = new Vue({
+var vueApp = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    nickname: '',
+    roomId: '',
+    microphoneActivable: false,
+    cameraActivable: false,
   },
-})
+  methods: {
+    microphoneSwitch: function () {
+      this.microphoneActivable = !this.microphoneActivable
+    },
+    cameraSwitch: function () {
+      this.cameraActivable = !this.cameraActivable
+    },
 
-var app2 = new Vue({
-  el: '#app-2',
-  data: {
-    message: '页面加载于 ' + new Date().toLocaleString()
+    quit: function () {
+      if (!confirm("确定退出吗？"))
+        return
+
+      alert('退出！')
+    },
+    createMeeting: async function () {
+      alert('')
+      return
+      const res = await axios.get('http://localhost:8000/newRoom')
+      var ret = res.data
+      if (ret.err) {
+        console.error(ret.err)
+        return
+      }
+
+      var roomId = ret.val
+      console.log('房间号：' + roomId)
+
+      res = await axios.get('http://localhost:8000/joinRoom', {
+        params: { roomId: roomId, nickname: '张三' }
+      })
+
+      if (ret.err) {
+        console.error(ret.err)
+        return
+      }
+
+      var server = res.data.val
+      console.log('服务端：' + server)
+    },
+    joinMeeting: async function () {
+      alert('join')
+    },
   }
 })
-
-var axios = require('axios').default
-
-//创建会议
-window.createMeeting = async function () {
-    const res = await axios.get('http://localhost:8000/newRoom')
-    var ret = res.data
-    if (ret.err) {
-        console.error(ret.err)
-        return
-    }
-
-    var roomId = ret.val
-    console.log('房间号：' + roomId)
-    
-    res = await axios.get('http://localhost:8000/joinRoom', {
-        params: { roomId: roomId, nickname: '张三' }
-    })
-
-    if (ret.err) {
-        console.error(ret.err)
-        return
-    }
-
-    var server = res.data.val
-    console.log('服务端：' + server)
-}
-
-//加入会议
-window.joinMeeting = async function () {
-    alert('')
-}
